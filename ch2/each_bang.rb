5ca1939a43bb747b27996ef16a01b892a5c9c214
David Doolin <david.doolin@gmail.com>
2016-03-24 04:29:42 -0700

    remove list elements after processing

--- ch2/each_bang.rb
+++ ch2/each_bang.rb
@@ -13,3 +13,15 @@ end
 list = nil
 GC.start
 puts ObjectSpace.each_object(Thing).count
+
+list = Array.new(1000) { Thing.new }
+puts ObjectSpace.each_object(Thing).count
+
+while list.count > 0
+  GC.start # this will garbage collect from the previous iteration
+  puts ObjectSpace.each_object(Thing).count # watch the counter decreasing
+  item = list.shift
+end
+
+GC.start # this will garbage collect from the previous iteration
+puts ObjectSpace.each_object(Thing).count # watch the counter decreasing
