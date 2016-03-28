772f6d93ae44636b7cba83db0b406ce318e1fd7c
David Doolin <david.doolin@gmail.com>
2016-03-28 04:33:50 -0700

    date parsing

---
+++ ch2/date_parsing1.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'date'
+require 'benchmark'
+
+date = '2014-05-23'
+time = Benchmark.realtime do
+  100_000.times do
+    Date.parse(date)
+  end
+end
+puts "%.3f" % time
