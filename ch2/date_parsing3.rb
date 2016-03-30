1df58e80294ef894ca112530299bbf3c4530b265
David Doolin <david.doolin@gmail.com>
2016-03-30 06:36:46 -0700

    date 3

---
+++ ch2/date_parsing3.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'date'
+require 'benchmark'
+
+date = "2014-05-23"
+time = Benchmark.realtime do
+  100_000.times do
+    Date.civil(date[0,4].to_i, date[5,2].to_i, date[8,2].to_i)
+  end
+end
+puts "%.3f" % time
