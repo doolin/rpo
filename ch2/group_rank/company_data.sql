f52cc8fe0448ea3daf5dbd5c0f6f2aea5ade0778
David Doolin <david.doolin@gmail.com>
2016-04-05 06:36:56 -0700

    hooked up makefile for company data

--- ch2/group_rank/company_data.sql
+++ ch2/group_rank/company_data.sql
@@ -8,3 +8,5 @@ insert into empsalaries (
   select (1 + round(random()*25)), *, (50000 + round(random()*250000))
   from generate_series(1, 10000)
 );
+
+create index empsalaries_department_idx on empsalaries (department_id);
