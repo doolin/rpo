b6664a23a861befdaaa7da2f4b9f1883172bd473
David Doolin <david.doolin@gmail.com>
2016-04-06 04:50:08 -0700

    p. 35 active record example

--- ch2/group_rank/group_rank.rb
+++ ch2/group_rank/group_rank.rb
@@ -12,4 +12,17 @@ class Empsalary < ActiveRecord::Base
   attr_accessor :rank
 end
 
+time = Benchmark.realtime do
+  salaries = Empsalary.all.order(:department_id, :salary)
 
+  key, counter = nil, nil
+  salaries.each do |s|
+    if s.department_id != key
+      key, counter = s.department_id, 0
+    end
+    counter += 1
+    s.rank = counter
+  end
+end
+
+puts "Group rank with ActiveRecord: %5.3fs" % time
