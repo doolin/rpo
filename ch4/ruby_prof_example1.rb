b189d21e681366551e2134e3106d6d34fd9e46e6
David Doolin <david.doolin@gmail.com>
2016-04-08 06:43:31 -0700

    daily

---
+++ ch4/ruby_prof_example1.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'date'
+require 'rubygems'
+require 'ruby-prof'
+
+GC.disable
+
+RubyProf.start
+Date.parse("2014-07-01")
+result = RubyProf.stop
+
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(File.open('ruby_prof_example_api1.txt', 'w+'))
