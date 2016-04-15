2b554fb659567d4e90781f98702b915f38933d93
David Doolin <david.doolin@gmail.com>
2016-04-15 05:49:42 -0700

    p. 60 various profiles

--- ch4/app.rb
+++ ch4/app.rb
@@ -37,3 +37,17 @@ def find_youngest(people)
 end
 
 data = generate_test_data
+GC.disable
+result = RubyProf.profile do
+  people = parse_data(data)
+  find_youngest(people)
+end
+
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(File.open("app_flat_profile.txt", "w+"), min_percent: 3)
+
+printer = RubyProf::GraphPrinter.new(result)
+printer.print(File.open("app_graph_profile.txt", "w+"), min_percent: 3)
+
+printer = RubyProf::CallStackPrinter.new(result)
+printer.print(File.open("app_call_stack_profile.txt", "w+"))
