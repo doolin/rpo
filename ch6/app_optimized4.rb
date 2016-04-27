0fa2b42cc36288c064b3a34b3793218528168dce
David Doolin <david.doolin@gmail.com>
2016-04-27 06:06:41 -0700

    in a big hurry today

--- ch6/app_optimized4.rb
+++ ch6/app_optimized4.rb
@@ -6,3 +6,11 @@ require 'date'
 # 1, John McTest, 1980-07-01
 # 2, Peter McGregor, 1985-12-23
 # 3, Sylvia MvIntosh, 1989-06-13
+
+def generate_test_data
+  50_000.times.map do |i|
+    name = ["John", "Peter", "Sylvia"][rand(3)] + " " +
+      ["McTest", "McGregor", "McIntosh"][rand(3)]
+    [i, name, Time.at(rand * Time.now.to_i).strftime("%Y-%m-%d") ].join(',')
+  end.join("\n")
+end
