9551b1815873e281b440f54c762b8d8f301aeeb3
David Doolin <david.doolin@gmail.com>
2016-04-29 06:41:10 -0700

    p. 87 start of valgrind example

--- ch6/app_optimized4.rb
+++ ch6/app_optimized4.rb
@@ -14,3 +14,20 @@ def generate_test_data
     [i, name, Time.at(rand * Time.now.to_i).strftime("%Y-%m-%d") ].join(',')
   end.join("\n")
 end
+
+def parse_data(data)
+  data.split("\n").map { | row| parse_row(row) }
+end
+
+def parse_row(row)
+  col1, col2, col3 = row.split(",")
+  [
+    col1.to_i,
+    col2,
+    Date.new(col3[0,4].to_i, col3[5,2].to_i, col3[8,2].to_i)
+  ]
+end
+
+def find_youngest(people)
+  people.map! { |person| person[2] }.max
+end
